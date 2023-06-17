da0fb57f276a732a55af721c6d68bd079f148190
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-06-17 20:03:17 +0530

    Nothing

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,28 @@ const BlogHeading = styled.h1`
     background-color:yellowgreen;
 `;
 
+const Post = styled.div`
+    border: 1px solid #e1e1e1;
+    padding: 10px 10px;
+    border-radius: 5px;
+    margin-top: 10px;
+
+  &:hover {
+    border:1px solid #2196f3;
+  }
+  p{
+      color:grey;
+      font-size: 13px;
+  }
+  a{
+    margin: 0;
+    padding: 0;
+    font-size: 25px;
+    font-weight: bold;
+    color: black;
+  }
+`;
+
 export default function Home() {
   const [posts, setPosts] = useState([]);
 
@@ -29,12 +51,12 @@ export default function Home() {
       <div id='blog-by'>JITU</div>
 
       {posts.map((post, index) => (
-        <div className='post' key={`post-${index}`} >
+        <Post className='post' key={`post-${index}`} >
           <Link to={`/post/${post.id}`}>
             <h3>{post.title}</h3>
           </Link>
           <p>{post.subTitle}</p>
-        </div>
+        </Post>
       ))}
 
     </div>
